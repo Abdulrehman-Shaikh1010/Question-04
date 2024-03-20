5baf8f0d1a797727c36d2456caa0270802886af8
Abdulrehman <you@example.com>
2024-03-20 14:59:12 +0500

    Question-04

---
+++ main.js
@@ -0,0 +1,7 @@
+/*find a quote from a famous person you admire. print the quote and the name of its Author. your output should
+look something like the following. including the quotation marks:
+Hazrat Muhammad SAW one said “Those people who show no mercy will receive no mercy from Allah.”
+*/
+var myQuote = "Hazrat Muhammad SAW one said,\u201CThose people who show no mercy will receive\n no mercy from Allah.\u201D";
+//backtick
+console.log(myQuote);
---
+++ main.ts
@@ -0,0 +1,11 @@
+/*find a quote from a famous person you admire. print the quote and the name of its Author. your output should 
+look something like the following. including the quotation marks:
+Hazrat Muhammad SAW one said “Those people who show no mercy will receive no mercy from Allah.” 
+*/
+
+const myQuote: string = `Hazrat Muhammad SAW one said,“Those people who show no mercy will receive
+ no mercy from Allah.”`
+
+ //backtick
+ 
+ console.log(myQuote);
